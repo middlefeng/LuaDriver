ff4a10b16d2dbe81165b9b85557faead32a9e40c
Dong Feng <middle.fengdong@gmail.com>
2013-08-09 14:04:52 +0800

    Refactor PART2: Factor out VertexArrayObject to other file.

--- LuaDriver/LuaOpenGLView.lua
+++ LuaDriver/LuaOpenGLView.lua
@@ -20,11 +20,11 @@ local OpenGLFrame = require "LuaOpenGLFrame"
 local OpenGLFrustum = require "LuaOpenGLFrustum"
 local OpenGLShaderManager = require "LuaOpenGLShaderManager"
 local OpenGLTransPipeline = require "LuaOpenGLTransformPipeline"
+local VertArrayObject = require "LuaOpenGLVertexObject"
 
 LDOpenGLView = {}
 
 local LDUtilities = {}
-local VertArrayObject = {}
 local TriangleBatch = {}
 
 
@@ -383,76 +383,6 @@ function TriangleBatch:draw()
 
 	NSOpenGL.bindBuffer("GL_ELEMENT_ARRAY_BUFFER", self.indexBuffer)
 	NSOpenGL.drawElements("GL_TRIANGLES", #self.indexes, "GL_UNSIGNED_SHORT");
-end
-
-
-
-
--------------------------------------------------------------------------
-------------------             Vertex Array            ------------------
--------------------------------------------------------------------------
-
-
-
-
-function VertArrayObject:__gc()
-	for _, buffer in pairs(self.buffers) do
-		NSOpenGL.deleteBuffers(buffer.handle)
-	end
-	NSOpenGL.deleteVertexArrays(self.vertObj)
-end
-
-
-
-
-function VertArrayObject:new()
-	self.__index = VertArrayObject
-	local result = {}
-	setmetatable(result, self)
-
-	result.vertObj = NSOpenGL.genVertexArrays(1)
-	result:bind()
-	result.buffers = {}
-
-	return result
-end
-
-
-
-function VertArrayObject:bind()
-	NSOpenGL.bindVertexArray(self.vertObj)
-end
-
-
-
-function VertArrayObject:unbind()
-	NSOpenGL.bindVertexArray(0)
-end
-
-
-
-function VertArrayObject:genBuffer(name, verts, purpose, normalized)
-	purpose = purpose or "GL_DYNAMIC_DRAW"
-	normalized = normalized or false
-
-	self.buffers[name] = {}
-	self.buffers[name].handle = NSOpenGL.genBuffers(1)
-	self.buffers[name].stride = #verts[1]
-	NSOpenGL.bindBuffer("GL_ARRAY_BUFFER", self.buffers[name].handle)
-	NSOpenGL.bufferData("GL_ARRAY_BUFFER", "GL_FLOAT", verts, purpose)
-
-	self.buffers[name].normalized = normalized
-end
-
-
-
-
-function VertArrayObject:bindBufferToAttrib(name, progName, attribName)
-	local location = OpenGLShaderManager.getAttribLocation(progName, attribName)
-	NSOpenGL.bindBuffer("GL_ARRAY_BUFFER", self.buffers[name].handle)
-	NSOpenGL.enableVertexAttribArray(location);
-	NSOpenGL.vertexAttribPointer(location, self.buffers[name].stride, "GL_FLOAT",
-								 self.buffers[name].normalized)
 end
 
 
